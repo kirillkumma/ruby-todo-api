e18d393e33eea2194d7c948a8b6354f24abe1026
Kirill Kumma <kirill.kumma@gmail.com>
2022-02-09 01:36:15 +0300

    db: migrate (replace password with password_digest in User)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_08_215135) do
+ActiveRecord::Schema.define(version: 2022_02_08_223409) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,10 +27,10 @@ ActiveRecord::Schema.define(version: 2022_02_08_215135) do
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "name"
-    t.string "password"
     t.string "string"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "todos", "users"
