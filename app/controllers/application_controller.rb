7fb9033bcb14804aea73397b6cfe73350ae8f283
Kirill Kumma <kirill.kumma@gmail.com>
2022-02-10 01:42:11 +0300

    controllers: add authorized method to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::API
+  private
+
+  def authorize
+    token = request.headers['Authorization']
+
+    if token
+      token = token.split.last
+      payload = AuthService.decode(token)
+      @current_user = User.find_by!(id: payload['id'])
+    else
+      render json: { error: { message: 'Unauthorized' } }, status: :unauthorized
+    end
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: { message: 'Invalid token' } },
+           status: :unauthorized
+  rescue JWT::ExpiredSignature
+    render json: { error: { message: 'Token has expired' } },
+           status: :unauthorized
+  rescue JWT::DecodeError
+    render json: { error: { message: 'Inavlid token' } }, status: :unauthorized
+  end
 end
