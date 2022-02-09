bc2d5c5525e210b125a6b8cc1847e4610ee43711
Kirill Kumma <kirill.kumma@gmail.com>
2022-02-10 01:43:05 +0300

    routes: add auth routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :todos
+      post 'register', to: 'auth#register'
+      post 'login', to: 'auth#login'
+      post 'authenticate', to: 'auth#authenticate'
     end
   end
 end
-
