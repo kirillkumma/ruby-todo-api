f7d4f82ee2dfcf105a3d5d42365c47428e51175c
Kirill Kumma <kirill.kumma@gmail.com>
2022-02-10 01:41:25 +0300

    models: add validations to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  has_secure_password
+  validates :email, presence: { message: "Can't be blank" },
+                    uniqueness: { message: 'Has already been taken' }
+  validates :email,
+            format: { with: URI::MailTo::EMAIL_REGEXP,
+                      message: 'Invalid format' }
+  validates :name, presence: { message: "Can't be blank" }
+  validates :password, presence: true,
+                       length: { minimum: 6, message: 'Too short (minimum is 6 characters)' }
 end
